9a57ef3a6de8beefb40c2df180cfd8e527bf05fa
Javier Cabero <3762851+reivash@users.noreply.github.com>
2024-02-27 18:17:02 +0100

    Test different position for new DungeonSpace

--- Assets/Scripts/DungeonSpaceController.cs
+++ Assets/Scripts/DungeonSpaceController.cs
@@ -13,6 +13,6 @@ public class DungeonSpace : MonoBehaviour {
         }
     }
     void ExpandDungeon() {
-        Instantiate(dungeonSpacePrefab, new Vector3(10, 10, 10), Quaternion.identity);
+        Instantiate(dungeonSpacePrefab, new Vector3(10, 0, 10), Quaternion.identity);
     }
 }
