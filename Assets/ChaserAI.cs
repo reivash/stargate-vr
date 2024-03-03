2c28cb80af0053c8ec42a25080f18c5d5bf201d0
Javier Cabero <3762851+reivash@users.noreply.github.com>
2024-03-03 10:01:52 +0100

    Working chaser enemy.

---
+++ Assets/ChaserAI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaserAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        if (playerInSightRange) ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+
+        print("Chasing player");
+        agent.SetDestination(player.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
