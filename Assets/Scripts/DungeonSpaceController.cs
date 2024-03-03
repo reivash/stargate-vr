b44b3e6682fad35660ada7e293f09de5163b1e38
Javier Cabero <3762851+reivash@users.noreply.github.com>
2024-03-03 16:39:43 +0100

    Added depth first traversal for tiles generation. Only visible in Debug console.

--- Assets/Scripts/DungeonSpaceController.cs
+++ Assets/Scripts/DungeonSpaceController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class DungeonSpace : MonoBehaviour
@@ -12,6 +15,11 @@ public class DungeonSpace : MonoBehaviour
     public static int HALF_DUNGEON_SIZE = DUNGEON_SIZE >> 1;
     public static int DUNGEON_ROOM_SIZE = 5;
     public static bool[,] matrix = new bool[DUNGEON_SIZE, DUNGEON_SIZE];
+
+    enum TILE_TYPE { UNASSIGNED, PATH_START, PATH_END, PATH, TRAP };
+
+    public static int TILES_SIDE = 5;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
@@ -46,8 +54,118 @@ public class DungeonSpace : MonoBehaviour
                         new Vector3(GetTransformValueFromDungeonIndex(x), 
                         0, 
                         GetTransformValueFromDungeonIndex(z)), Quaternion.identity);
+            CreateFloorPuzzleDungeon();
         }
     }
+
+    private void CreateFloorPuzzleDungeon() {
+        // Part 1: Generate maze with one possible path. Mark the rest as traps.
+        TILE_TYPE[,] tiles = new TILE_TYPE[TILES_SIDE, TILES_SIDE];
+
+        // Initialize area.
+        for (int i = 0; i < TILES_SIDE; i++)
+        {
+            for (int j = 0; j < TILES_SIDE; j++) {
+                tiles[i, j] = TILE_TYPE.UNASSIGNED;
+            }
+        }
+        tiles[0, TILES_SIDE / 2] = TILE_TYPE.PATH_START;
+        tiles[TILES_SIDE - 1, TILES_SIDE / 2] = TILE_TYPE.PATH_END;
+
+        Stack<(int x, int z)> path = new Stack<(int x, int z)>();
+        int x = 0, z = TILES_SIDE / 2;
+
+        List<(int x, int z)> moveOptions = new List<(int x, int z)>();
+        while (true) {
+            // If final cell, break.
+            if (tiles[x, z] == TILE_TYPE.PATH_END) {
+                print("Final tile reached");
+                break;
+            }
+
+            // Compute move options at current position.
+            if (0 < x)
+                if (IsValidTileForPath(tiles[x - 1, z]))
+                    moveOptions.Add((x - 1, z));
+            if (0 < z)
+                if (IsValidTileForPath(tiles[x, z - 1]))
+                    moveOptions.Add((x, z - 1));
+            if (x < (TILES_SIDE - 1))
+                if (IsValidTileForPath(tiles[x + 1, z ]))
+                    moveOptions.Add((x + 1, z));
+            if (z < (TILES_SIDE - 1))
+                if (IsValidTileForPath(tiles[x, z + 1]))
+                    moveOptions.Add((x, z + 1));
+
+            // If no more move options, check cell as TRAP. Go back.
+            if (moveOptions.Count == 0)
+            {
+                tiles[x, z] = TILE_TYPE.TRAP;
+
+                if (path.Count == 0)
+                {
+                    print("Something went wrong.");
+                    break;
+                }
+
+                (x, z) = path.Pop();
+            } else { 
+                // If move options, check cell as path and mark as path.
+                tiles[x, z] = TILE_TYPE.PATH;
+                path.Push((x, z));
+                int randomNumber = Random.Range(0, moveOptions.Count);
+                (x, z) = moveOptions[randomNumber];
+                moveOptions.Clear();
+            }
+        }
+
+        
+        // Mark every unassigned as trap.
+        for (int i = 0; i < TILES_SIDE; i++)
+        {
+            for (int j = 0; j < TILES_SIDE; j++)
+            {
+                if (tiles[i, j] == TILE_TYPE.UNASSIGNED)
+                    tiles[i, j] = TILE_TYPE.TRAP;
+            }
+        }
+
+        // Debug: print
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < TILES_SIDE; i++)
+        {
+            for (int j = 0; j < TILES_SIDE; j++)
+            {
+                switch (tiles[i, j])
+                {
+                    case TILE_TYPE.TRAP:
+                        stringBuilder.Append("X");
+                        break;
+                    case TILE_TYPE.PATH:
+                        stringBuilder.Append("O");
+                        break;
+                    case TILE_TYPE.PATH_START:
+                        stringBuilder.Append("S");
+                        break;
+                    case TILE_TYPE.PATH_END:
+                        stringBuilder.Append("E");
+                        break;
+                    default:
+                        stringBuilder.Append("?");
+                        break;
+                }
+            }
+            stringBuilder.Append("\n");
+        }
+        print(stringBuilder.ToString());
+
+        // Part 2: Generate GameObjects.
+    }
+
+    bool IsValidTileForPath(TILE_TYPE tileType) {
+        return tileType == TILE_TYPE.UNASSIGNED || tileType == TILE_TYPE.PATH_END;
+    }
+
     int GetDungeonIndexFromTransformValue(float transform_value)
     {
         return (int)transform_value / DUNGEON_ROOM_SIZE + HALF_DUNGEON_SIZE;
