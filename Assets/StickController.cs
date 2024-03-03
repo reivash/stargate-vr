6db424a823a9427fca184e4d5fdb525eadef8e22
Javier Cabero <3762851+reivash@users.noreply.github.com>
2024-03-03 10:25:14 +0100

    Stick does not damage if not grabbed.

--- Assets/StickController.cs
+++ Assets/StickController.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class StickController : MonoBehaviour
 {
     public int stickDamage = 4;
 
+
+    private void Awake()
+    {
+        this.enabled = false;
+        XRGrabInteractable xRGrabInteractable = GetComponent<XRGrabInteractable>();
+        xRGrabInteractable.selectEntered.AddListener(OnGrabbed);
+        xRGrabInteractable.selectExited.AddListener(OnReleased);
+    }
+
+    private void OnGrabbed(SelectEnterEventArgs arg)
+    {
+        this.enabled = true;
+    }
+
+    private void OnReleased(SelectExitEventArgs arg)
+    {
+        this.enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        print("Entered trigger: " + other.name);
-        if (other.name == "ChaserCapsule")
+        if (enabled && other.name == "ChaserCapsule")
         {
             ChaserAI script = GameObject.Find("ChaserCapsule").GetComponent<ChaserAI>();
             script.TakeDamage(stickDamage);
