8cbc578869a0a1482e125248536328345ee17d77
Javier Cabero <3762851+reivash@users.noreply.github.com>
2024-04-14 19:33:54 +0200

    Add rock to room decorator.

--- Assets/Scripts/RoomDecorator.cs
+++ Assets/Scripts/RoomDecorator.cs
@@ -6,12 +6,19 @@ using static Coordinates;
 
 public class RoomDecorator : MonoBehaviour {
     public GameObject crates;
+    public GameObject rock;
 
     public void DecorateRoom(int dungeonX, int dungeonZ) {
+        float x = GetTransformValueFromDungeonIndex(dungeonX);
+        float z = GetTransformValueFromDungeonIndex(dungeonZ);
+
+        // Randomize props.
         if (Random.value > .5) {
-            float x = GetTransformValueFromDungeonIndex(dungeonX);
-            float z = GetTransformValueFromDungeonIndex(dungeonZ);
-            Instantiate(crates, new Vector3(x, 0, z), Quaternion.identity);
+            Instantiate(crates, new Vector3(x, 0, z), Quaternion.AngleAxis(90 * (int)(Random.value * 4), Vector3.up));
+        } else if (Random.value > 0) {
+            Instantiate(rock, new Vector3(x, 0, z), Quaternion.AngleAxis(90 * (int)(Random.value * 4), Vector3.up));
         }
+
+        // TODO: Randomize lights.
     }
 }
