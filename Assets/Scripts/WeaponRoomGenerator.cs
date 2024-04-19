babe36b7ac622fcc85804d84d85caceed355549e
Javier Cabero <3762851+reivash@users.noreply.github.com>
2024-04-19 20:38:59 +0200

    Add cone light to weapon room.

--- Assets/Scripts/WeaponRoomGenerator.cs
+++ Assets/Scripts/WeaponRoomGenerator.cs
@@ -12,5 +12,16 @@ public class WeaponRoomGenerator : RoomGenerator
         float x = GetTransformValueFromDungeonIndex(dungeonX);
         float z = GetTransformValueFromDungeonIndex(dungeonZ);
         Instantiate(weaponPrefab, new Vector3(x, 1, z), Quaternion.identity);
+        InstantiateConeLight(x, z);
+    }
+
+    private void InstantiateConeLight(float x, float z) {
+        GameObject lightGameObject = new GameObject("WeaponRoomConeLight");
+        Light lightComp = lightGameObject.AddComponent<Light>();
+        lightComp.type = LightType.Spot;
+        lightGameObject.transform.position = new Vector3(x, 3, z);
+        lightGameObject.transform.rotation = Quaternion.Euler(90, 0, 0);
+        lightComp.range = 5;
+        lightComp.intensity = 15;
     }
 }
